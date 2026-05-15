4cb1ede638ec06d72458cfe3ef0d3a9415c5f04b
Tiji <143082629+IamTiji@users.noreply.github.com>
2026-05-15 23:48:45 +0800

    Use jextract instead of JCEF
    The reason for the switch is because JCEF is full of deprecated APIs, which is impossible for a single person to fix. There is finalize everywhere, and new Boolean() is very commonly found. It would be fine if we used old Java, but since we are using modern Java, this is not sustainable.
    
    JavaCPP was considered when switching, but as the maintainer seemed to not care about fixing problems, it was quite quickly scrapped.
    
    SWIG was a great alternative, but it didn't support common compiler specific keywords.
    
     Also, this is most cursed commit that I will ever make in my life.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.nio.file.Files
+
 plugins {
     id("net.fabricmc.fabric-loom-remap") version "1.15-SNAPSHOT"
     id("maven-publish")
@@ -27,8 +29,103 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")!!}")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")!!}")
+}
 
-    implementation(files("./jcef/jcef.jar"))
+fun getPlatformName(): String {
+    var platform = System.getProperty("os.name").lowercase()
+    platform =
+        if (platform.startsWith("windows")) "windows"
+        else if (platform.startsWith("mac")) "macos"
+        else if (platform.startsWith("linux")) "linux"
+        else throw IllegalArgumentException("Unsupported operating system: ${System.getProperty("os.name")}")
+
+    val osArch = when (System.getProperty("os.arch").lowercase()) {
+        "amd64", "x86_64" -> "x64"
+        "aarch64", "arm64", "arm" -> "arm64"
+        "x86", "i386", "i686" -> "x86"
+        else -> throw IllegalArgumentException("Unsupported arch: ${System.getProperty("os.arch")}")
+    }
+
+    val platformMap = mapOf(
+        ("windows" to "x64") to "windows64",
+        ("windows" to "arm64") to "windowsarm64",
+        ("windows" to "x86") to "windows32",
+
+        ("macos" to "x64") to "macosx64",
+        ("macos" to "arm64") to "macosarm64",
+
+        ("linux" to "x64") to "linux64",
+        ("linux" to "arm64") to "linuxarm64"
+    )
+
+    return platformMap[platform to osArch]?: throw IllegalArgumentException("Unsupported platform: $platform")
+}
+
+tasks.register("getCEF") {
+    val cefVersion = "147.0.14+g76d2442+chromium-147.0.7727.138"
+    val cefReleaseCDN = "https://cef-builds.spotifycdn.com/"
+
+    val url = "${cefReleaseCDN}cef_binary_${cefVersion}_${getPlatformName()}.tar.bz2"
+    val file = Files.createTempFile("cef", ".tar.bz2")
+    ant.invokeMethod("get", mapOf("src" to url, "dest" to file, "verbose" to "true"))
+
+    copy {
+        from(tarTree(resources.bzip2(file))) {
+            eachFile {
+                path = path.substringAfter("/")
+            }
+        }
+        into("./cef")
+    }
+    delete(file)
+}
+
+tasks.register("genMasterHeader") {
+    val lookIn = arrayOf("include/capi")
+    val files = mutableListOf<File>()
+
+    lookIn.forEach { files.addAll(File("cef/$it").listFiles()) }
+    val header = StringBuilder("""
+        // This file is generated! It will get overwritten if you modify it. 
+        // If you insist on editing this, don't cry later...
+        //
+        // If you want to regenerate this file, run: gradlew genMasterHeader
+        
+        
+        #ifndef SLICEF_CEF_MASTER_HEADER_
+        #define SLICEF_CEF_MASTER_HEADER_
+        
+        
+    """.trimIndent())
+
+    files.forEach {
+        if (it.extension != "h") return@forEach
+        header.append("#include <${it.path.substringAfter("cef/")}>\n")
+    }
+    header.append("\n#endif // SLICEF_CEF_MASTER_HEADER_")
+
+    val file = File("src/main/headers/cef_master.h")
+    if (file.exists()) delete(file)
+    file.createNewFile()
+    file.writeText(header.toString())
+}
+
+tasks.register<Exec>("genNatives") {
+    doFirst {
+        val folder = File("src/main/java/com/tiji/silcef/natives")
+        if (!folder.exists()) return@doFirst
+        folder.listFiles()?.forEach { file -> if (file.nameWithoutExtension != "package-info") file.delete() }
+    }
+
+    val isWindows = System.getProperty("os.name").lowercase().contains("windows")
+    commandLine(if (isWindows) "jextract.bat" else "jextract")
+    args(
+        "-l", ":cef/Release/libcef.lib",
+        "--output", "src/main/java/",
+        "-t", "com.tiji.silcef.natives",
+        "-I", "cef",
+        "src/main/headers/cef_master.h"
+    )
 }
 
 val targetJavaVersion = 21
---
+++ src/main/java/com/tiji/silcef/natives/package-info.java
@@ -0,0 +1,4 @@
+package com.tiji.silcef.natives;
+
+/// This contains classes mapped to native C++ CEF.
+/// If you modify this, it will be overwritten; DO NOT EDIT!
