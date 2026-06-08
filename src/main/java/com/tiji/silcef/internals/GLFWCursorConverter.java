671544fa440637612ef8114b268575a98e411bab
Tiji <143082629+IamTiji@users.noreply.github.com>
2026-06-08 21:03:16 +0800

    Make the name of cursor consistent

--- src/main/java/com/tiji/silcef/internals/GLFWCursorConverter.java
+++ src/main/java/com/tiji/silcef/internals/GLFWCursorConverter.java
@@ -12,36 +12,36 @@ import static org.lwjgl.glfw.GLFW.*;
  */
 public class GLFWCursorConverter implements CursorConverter {
     private static final Map<Integer, CursorType> cursorMap = Map.ofEntries(
-            Map.entry(CT_POINTER,            CursorType.createStandardCursor(GLFW_ARROW_CURSOR,         "slicef_CT_POINTER",          CursorType.DEFAULT)),
-            Map.entry(CT_CROSS,              CursorType.createStandardCursor(GLFW_CROSSHAIR_CURSOR,     "slicef_CT_CROSS",            CursorType.DEFAULT)),
-            Map.entry(CT_HAND,               CursorType.createStandardCursor(GLFW_POINTING_HAND_CURSOR, "slicef_CT_HAND",             CursorType.DEFAULT)),
-            Map.entry(CT_IBEAM,              CursorType.createStandardCursor(GLFW_IBEAM_CURSOR,         "slicef_CT_IBEAM",            CursorType.DEFAULT)),
-            Map.entry(CT_WAIT,               CursorType.createStandardCursor(GLFW_ARROW_CURSOR,         "slicef_CT_WAIT",             CursorType.DEFAULT)),
-            Map.entry(CT_HELP,               CursorType.createStandardCursor(GLFW_ARROW_CURSOR,         "slicef_CT_HELP",             CursorType.DEFAULT)),
+            Map.entry(CT_POINTER,            CursorType.createStandardCursor(GLFW_ARROW_CURSOR,         "CT_POINTER",          CursorType.DEFAULT)),
+            Map.entry(CT_CROSS,              CursorType.createStandardCursor(GLFW_CROSSHAIR_CURSOR,     "CT_CROSS",            CursorType.DEFAULT)),
+            Map.entry(CT_HAND,               CursorType.createStandardCursor(GLFW_POINTING_HAND_CURSOR, "CT_HAND",             CursorType.DEFAULT)),
+            Map.entry(CT_IBEAM,              CursorType.createStandardCursor(GLFW_IBEAM_CURSOR,         "CT_IBEAM",            CursorType.DEFAULT)),
+            Map.entry(CT_WAIT,               CursorType.createStandardCursor(GLFW_ARROW_CURSOR,         "CT_WAIT",             CursorType.DEFAULT)),
+            Map.entry(CT_HELP,               CursorType.createStandardCursor(GLFW_ARROW_CURSOR,         "CT_HELP",             CursorType.DEFAULT)),
 
             // Directional Resizing
-            Map.entry(CT_EASTRESIZE,         CursorType.createStandardCursor(GLFW_RESIZE_EW_CURSOR,     "slicef_CT_EASTRESIZE",       CursorType.DEFAULT)),
-            Map.entry(CT_NORTHRESIZE,        CursorType.createStandardCursor(GLFW_RESIZE_NS_CURSOR,     "slicef_CT_NORTHRESIZE",      CursorType.DEFAULT)),
-            Map.entry(CT_NORTHEASTRESIZE,    CursorType.createStandardCursor(GLFW_RESIZE_NESW_CURSOR,   "slicef_CT_NORTHEASTRESIZE",  CursorType.DEFAULT)),
-            Map.entry(CT_NORTHWESTRESIZE,    CursorType.createStandardCursor(GLFW_RESIZE_NWSE_CURSOR,   "slicef_CT_NORTHWESTRESIZE",  CursorType.DEFAULT)),
-            Map.entry(CT_SOUTHRESIZE,        CursorType.createStandardCursor(GLFW_RESIZE_NS_CURSOR,     "slicef_CT_SOUTHRESIZE",      CursorType.DEFAULT)),
-            Map.entry(CT_SOUTHEASTRESIZE,    CursorType.createStandardCursor(GLFW_RESIZE_NWSE_CURSOR,   "slicef_CT_SOUTHEASTRESIZE",  CursorType.DEFAULT)),
-            Map.entry(CT_SOUTHWESTRESIZE,    CursorType.createStandardCursor(GLFW_RESIZE_NESW_CURSOR,   "slicef_CT_SOUTHWESTRESIZE",  CursorType.DEFAULT)),
-            Map.entry(CT_WESTRESIZE,         CursorType.createStandardCursor(GLFW_RESIZE_EW_CURSOR,     "slicef_CT_WESTRESIZE",       CursorType.DEFAULT)),
+            Map.entry(CT_EASTRESIZE,         CursorType.createStandardCursor(GLFW_RESIZE_EW_CURSOR,     "CT_EASTRESIZE",       CursorType.DEFAULT)),
+            Map.entry(CT_NORTHRESIZE,        CursorType.createStandardCursor(GLFW_RESIZE_NS_CURSOR,     "CT_NORTHRESIZE",      CursorType.DEFAULT)),
+            Map.entry(CT_NORTHEASTRESIZE,    CursorType.createStandardCursor(GLFW_RESIZE_NESW_CURSOR,   "CT_NORTHEASTRESIZE",  CursorType.DEFAULT)),
+            Map.entry(CT_NORTHWESTRESIZE,    CursorType.createStandardCursor(GLFW_RESIZE_NWSE_CURSOR,   "CT_NORTHWESTRESIZE",  CursorType.DEFAULT)),
+            Map.entry(CT_SOUTHRESIZE,        CursorType.createStandardCursor(GLFW_RESIZE_NS_CURSOR,     "CT_SOUTHRESIZE",      CursorType.DEFAULT)),
+            Map.entry(CT_SOUTHEASTRESIZE,    CursorType.createStandardCursor(GLFW_RESIZE_NWSE_CURSOR,   "CT_SOUTHEASTRESIZE",  CursorType.DEFAULT)),
+            Map.entry(CT_SOUTHWESTRESIZE,    CursorType.createStandardCursor(GLFW_RESIZE_NESW_CURSOR,   "CT_SOUTHWESTRESIZE",  CursorType.DEFAULT)),
+            Map.entry(CT_WESTRESIZE,         CursorType.createStandardCursor(GLFW_RESIZE_EW_CURSOR,     "CT_WESTRESIZE",       CursorType.DEFAULT)),
 
             // Double-ended Resizing
-            Map.entry(CT_NORTHSOUTHRESIZE,   CursorType.createStandardCursor(GLFW_RESIZE_NS_CURSOR,     "slicef_CT_NORTHSOUTHRESIZE", CursorType.DEFAULT)),
-            Map.entry(CT_EASTWESTRESIZE,     CursorType.createStandardCursor(GLFW_RESIZE_EW_CURSOR,     "slicef_CT_EASTWESTRESIZE",   CursorType.DEFAULT)),
-            Map.entry(CT_COLUMNRESIZE,       CursorType.createStandardCursor(GLFW_RESIZE_EW_CURSOR,     "slicef_CT_COLUMNRESIZE",     CursorType.DEFAULT)),
-            Map.entry(CT_ROWRESIZE,          CursorType.createStandardCursor(GLFW_RESIZE_NS_CURSOR,     "slicef_CT_ROWRESIZE",        CursorType.DEFAULT)),
+            Map.entry(CT_NORTHSOUTHRESIZE,   CursorType.createStandardCursor(GLFW_RESIZE_NS_CURSOR,     "CT_NORTHSOUTHRESIZE", CursorType.DEFAULT)),
+            Map.entry(CT_EASTWESTRESIZE,     CursorType.createStandardCursor(GLFW_RESIZE_EW_CURSOR,     "CT_EASTWESTRESIZE",   CursorType.DEFAULT)),
+            Map.entry(CT_COLUMNRESIZE,       CursorType.createStandardCursor(GLFW_RESIZE_EW_CURSOR,     "CT_COLUMNRESIZE",     CursorType.DEFAULT)),
+            Map.entry(CT_ROWRESIZE,          CursorType.createStandardCursor(GLFW_RESIZE_NS_CURSOR,     "CT_ROWRESIZE",        CursorType.DEFAULT)),
 
             // Movement and Status
-            Map.entry(CT_MOVE,               CursorType.createStandardCursor(GLFW_RESIZE_ALL_CURSOR,    "slicef_CT_MOVE",             CursorType.DEFAULT)),
-            Map.entry(CT_NOTALLOWED,         CursorType.createStandardCursor(GLFW_NOT_ALLOWED_CURSOR,   "slicef_CT_NOTALLOWED",       CursorType.DEFAULT)),
+            Map.entry(CT_MOVE,               CursorType.createStandardCursor(GLFW_RESIZE_ALL_CURSOR,    "CT_MOVE",             CursorType.DEFAULT)),
+            Map.entry(CT_NOTALLOWED,         CursorType.createStandardCursor(GLFW_NOT_ALLOWED_CURSOR,   "CT_NOTALLOWED",       CursorType.DEFAULT)),
 
             // Grabbing
-            Map.entry(CT_GRAB,               CursorType.createStandardCursor(GLFW_POINTING_HAND_CURSOR, "slicef_CT_GRAB",             CursorType.DEFAULT)),
-            Map.entry(CT_GRABBING,           CursorType.createStandardCursor(GLFW_POINTING_HAND_CURSOR, "slicef_CT_GRABBING",         CursorType.DEFAULT))
+            Map.entry(CT_GRAB,               CursorType.createStandardCursor(GLFW_POINTING_HAND_CURSOR, "CT_GRAB",             CursorType.DEFAULT)),
+            Map.entry(CT_GRABBING,           CursorType.createStandardCursor(GLFW_POINTING_HAND_CURSOR, "CT_GRABBING",         CursorType.DEFAULT))
     );
 
     @Override
